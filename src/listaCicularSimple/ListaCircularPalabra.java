928e0315d43de90e2bdc05f38b1e5fc45c116b60
Samuel Aroca <samuel.arocad@unac.edu.co>
2022-03-24 17:45:04 -0500

    kdas

--- src/listaCicularSimple/ListaCircularPalabra.java
+++ src/listaCicularSimple/ListaCircularPalabra.java
@@ -10,17 +10,43 @@ public class ListaCircularPalabra {
         ListaCircularSimple listaCp;
         BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
         listaCp = new ListaCircularSimple();
+        int opc;
         System.out.println("\n Entrada de Nombres. Termina con fin.\n");
         try {
             while (!(("fin").equals(palabra = entrada.readLine()))) {
                 //while ((palabra = entrada.readLine()) != null) {
                 //System.out.println("Leyó: " + palabra);
-                String nueva;
-                nueva = new String(palabra);
+                String nueva = new String(palabra);
                 listaCp.insertar(nueva);
             }
             System.out.println("\t\tLista circular de palabras");
             listaCp.recorrer();
+            System.out.println("\n\t Opciones para manejar la lista");
+            do {
+                System.out.println("1. Insertar una palabra.\n");
+                System.out.println("2. Eliminar una palabra.\n");
+                System.out.println("3. Mostrar la lista completa.\n");
+                System.out.println("4. Salir .\n");
+                do {
+                    opc = Integer.parseInt(entrada.readLine());
+                } while (opc < 1 || opc > 4);
+                switch (opc) {
+                    case 1:
+                        System.out.print("Palabra a insertar: ");
+                        palabra = entrada.readLine();
+                        listaCp.insertar(palabra);
+                        break;
+                    case 2:
+                        System.out.print("Palabra a eliminar: ");
+                        palabra = entrada.readLine();
+                        listaCp.eliminar(palabra);
+                        break;
+                    case 3:
+                        System.out.println("Palabras en la Lista:\n");
+                        listaCp.recorrer();
+                        break;
+                }
+            } while (opc != 4);
         } catch (Exception e) {
             System.out.println("Error\n");
         }
--- src/listaCicularSimple/ListaCircularSimple.java
+++ src/listaCicularSimple/ListaCircularSimple.java
@@ -1,6 +1,6 @@
 package listaCicularSimple;
 
-public class ListaCircularSimple {
+public class ListaCircularSimple{
     private Nodo lc;
 
     public ListaCircularSimple() {
@@ -9,25 +9,54 @@ public class ListaCircularSimple {
 
     public ListaCircularSimple insertar(String entrada) {
         Nodo nuevo = new Nodo(entrada);
-        if (lc != null) {
+        if (lc != null) { // lista circular no vacía
             nuevo.enlace = lc.enlace;
             lc.enlace = nuevo;
         }
         lc = nuevo;
+        //System.out.println("Insertado: " + entrada);
         return this;
+    }
+
+    public void eliminar(String entrada) {
+        Nodo actual = lc;
+        if (lc == null) {
+            return;
+        }
+        if (lc.dato.equals(entrada)) {
+            if (lc == lc.enlace) { // Lista con un solo nodo
+                lc = null;
+            } else {
+                lc = lc.enlace;
+            }
+            return;
+        }
+        while (actual.enlace != lc ) {
+            if (actual.enlace.dato.equals(entrada)) {
+                // Enlace de nodo anterior con el siguiente
+                // si se ha encontrado el nodo.
+                Nodo p = actual.enlace; // Nodo a eliminar
+                if (p == lc) {
+                    lc = actual; // Se borra el elemento referenciado por lc,
+                    // el nuevo acceso a la lista es el anterior
+                }
+                actual.enlace = p.enlace;
+            } else {
+                actual = actual.enlace;
+            }
+        }
     }
 
     public void recorrer() {
         Nodo p;
-        if (lc != null){
-            p = lc.enlace;
-            do {
-                System.out.println("\t" + p.dato);
+        if (lc != null) {
+            p = lc; // siguiente nodo al de acceso
+            System.out.println("\t" + p.dato);
+            while (p.enlace != lc) {
+                System.out.println("\t" + p.enlace.dato);
                 p = p.enlace;
-
-            } while (p != lc.enlace);
-        } else {
-            System.out.println("\t Lista cicular vacia.");
-        }
+            }
+        } else
+            System.out.println("\t Lista Circular vacía.");
     }
 }
